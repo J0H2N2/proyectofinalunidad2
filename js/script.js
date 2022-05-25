1ab4c9030aed1c81525df82c674a1892d38c16e1
J0H2N2 <leijodumo89@gmail.com>
2022-05-24 20:36:33 -0500

    actividad semana 5

---
+++ js/script.js
@@ -0,0 +1,10 @@
+alert("Educación en casa");
+prompt("Ingresa el nombre de la página");
+confirm("deseas salir de la pagina");
+
+var nombreSitio = prompt("como se llama este sitio web");
+if (nombreSitio =="Educación en casa"){
+    alert("El nombre es correcto");
+} else{
+    alert("El nombre es inconrrecto");
+}
